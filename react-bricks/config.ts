e28247e4c5184f64b9504e699e44e1ed382dbf22
DerickDu <dododugan@gmail.com>
2023-01-24 12:43:54 -0500

    update logo

--- react-bricks/config.ts
+++ react-bricks/config.ts
@@ -10,7 +10,7 @@ const config: types.ReactBricksConfig = {
   apiKey: process.env.API_KEY,
   pageTypes,
   bricks,
-  logo: '/logo.png',
+  logo: '/logo.svg',
   previewPath: '/preview',
   // contentClassName: 'content', // Defined dynamically
   // isDarkColorMode: ...,        // in _app.tsx
